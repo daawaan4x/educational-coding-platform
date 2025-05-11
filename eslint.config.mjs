b5e22c1f7f674b0d1cf29d644cce42fd0c153d2d
daawaan4x <theoneeclarin@outlook.com>
2025-05-11 19:51:17 +0800

    build(core): initialize repository

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import prettiereslint from "eslint-config-prettier/flat";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	tseslint.configs.recommendedTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		ignores: [
+			"node_modules",
+			"dist",
+			".next",
+			"out",
+			"src/components/magicui",
+			"tailwind.config.ts",
+			"eslint.config.mjs",
+			".prettierrc.js",
+		],
+	},
+	prettiereslint,
+);
