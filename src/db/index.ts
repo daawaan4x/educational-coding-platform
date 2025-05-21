485857c11d551e256c7fcfe896d83949ed5743bd
daawaan4x <theoneeclarin@outlook.com>
2025-05-21 15:26:16 +0800

    feat(database): add db schemas

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema";
 
-export const db = drizzle(postgres(process.env.DATABASE_URL));
+export const db = drizzle({ client: postgres(process.env.DATABASE_URL), schema });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,87 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { boolean, integer, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const testTable = pgTable("Test", {
-	id: uuid().primaryKey().defaultRandom(),
-	value: varchar({ length: 255 }),
+const baseFields = {
+	id: uuid().primaryKey().notNull().defaultRandom(),
+	date_created: timestamp().notNull().defaultNow(),
+	date_modified: timestamp().notNull().defaultNow(),
+};
+
+// MARK: TABLES
+
+export const users = pgTable("users", {
+	...baseFields,
+	email: varchar({ length: 255 }).notNull(),
+	last_name: varchar({ length: 255 }).notNull(),
+	first_name: varchar({ length: 255 }).notNull(),
 });
+
+export const classes = pgTable("classes", {
+	...baseFields,
+	name: varchar({ length: 255 }).notNull(),
+});
+
+export const problems = pgTable("problems", {
+	...baseFields,
+	name: varchar({ length: 255 }).notNull(),
+	description: text().notNull(),
+	deadline: timestamp().notNull(),
+	max_score: integer().notNull().default(0),
+	class_id: uuid()
+		.notNull()
+		.references(() => classes.id),
+	author_id: uuid()
+		.notNull()
+		.references(() => users.id),
+});
+
+export const solutions = pgTable("solutions", {
+	...baseFields,
+	submitted: boolean().notNull().default(false),
+	code: text().notNull().default(""),
+	score: integer(),
+	problem_id: uuid()
+		.notNull()
+		.references(() => problems.id),
+	author_id: uuid()
+		.notNull()
+		.references(() => users.id),
+});
+
+// MARK: RELATIONS
+
+export const user_relations = relations(users, ({ many }) => ({
+	classes: many(users_to_classes),
+	problems: many(problems),
+	solutions: many(solutions),
+}));
+
+export const class_relations = relations(classes, ({ many }) => ({
+	users: many(users_to_classes),
+	problems: many(problems),
+}));
+
+export const users_to_classes = pgTable("users_classes", {
+	user_id: uuid()
+		.notNull()
+		.references(() => users.id),
+	class_id: uuid()
+		.notNull()
+		.references(() => classes.id),
+});
+
+export const users_to_classes_relations = relations(users_to_classes, ({ one }) => ({
+	user: one(users, { fields: [users_to_classes.user_id], references: [users.id] }),
+	class: one(classes, { fields: [users_to_classes.class_id], references: [classes.id] }),
+}));
+
+export const problems_relations = relations(problems, ({ one, many }) => ({
+	class: one(classes, { fields: [problems.class_id], references: [classes.id] }),
+	author: one(users, { fields: [problems.author_id], references: [users.id] }),
+	solutions: many(solutions),
+}));
+
+export const solutions_relations = relations(solutions, ({ one }) => ({
+	problem: one(problems, { fields: [solutions.problem_id], references: [problems.id] }),
+	author: one(users, { fields: [solutions.author_id], references: [users.id] }),
+}));
