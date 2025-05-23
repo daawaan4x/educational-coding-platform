9b3119c4e9039b53d42a123b7e729554c16c9b0b
Luke <zionexodus7@protonmail.com>
2025-05-23 11:17:16 +0800

    build(ui): install sidebar-07

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { Login } from "@/components/login";
+
+export default function Page() {
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<Login />
+		</div>
+	);
+}
