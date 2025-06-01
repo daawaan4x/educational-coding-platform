6a3a5ff20cd494851f4da31e41e360f3a10f148b
Luke <zionexodus7@protonmail.com>
2025-06-01 16:58:33 +0800

    fix: relocated /login to /auth/login (not-authed)

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Login } from "@/components/ui/login";
-
-export default function Page() {
-	return (
-		<div className="flex h-screen items-center justify-center">
-			<Login />
-		</div>
-	);
-}
