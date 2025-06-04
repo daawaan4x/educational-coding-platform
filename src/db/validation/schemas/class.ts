df9c01db3804bbbf4e9e0393c907a9ed0cf1c8bf
Theone Eclarin <theoneeclarin@outlook.com>
2025-06-04 16:49:55 +0800

    fix(schema): change ID parsers to GUID to parse fake UUIDs for dev

--- src/db/validation/schemas/class.ts
+++ src/db/validation/schemas/class.ts
@@ -2,7 +2,7 @@ import z from "zod/v4";
 import { classes } from "../../schema";
 import { createInsertSchema, createSelectSchema, createUpdateSchema, metafields } from "../utils";
 
-export const Select = createSelectSchema(classes);
+export const Select = createSelectSchema(classes, { id: z.guid() });
 export type Select = z.output<typeof Select>;
 
 export const Insert = createInsertSchema(classes).omit(metafields);
